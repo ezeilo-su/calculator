c20717c32d3142064d16e1423b1b25230122ca73
ezeilo-su <ezeilo.su@gmail.com>
2021-03-19 23:47:22 +0100

    Create new app. Install app dependencies. Setup linters.

---
+++ src/components/Calculator.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const Calculator = () => {
+  return(
+    <h1>Sample Calculator</h1>
+  );
+}
+
+export default Calculator;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './components/Calculator'
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(
+  <Calculator />,
+  document.getElementById('root')
+);
