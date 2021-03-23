73723ae96bd83c4c2eabfd131228fd05dd2ca3e4
ezeilo-su <ezeilo.su@gmail.com>
2021-03-23 16:43:34 +0100

    Complete calculate module

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,20 +1,57 @@
 import operate from './operate';
 
-const calculate = ({ total, next, operation }, btnName) => {
-  let cTotal = total;
-  let cNext = next;
-  const cOperation = operation;
-  switch (btnName) {
+const calculate = (calcData, buttonName) => {
+  if (buttonName === 'AC') {
+    return {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  let { total, next, operation } = calcData;
+
+  switch (buttonName) {
     case '+/-':
-      cTotal *= -1;
-      cNext *= -1;
-      break;
+      return { total: next ? total : total * -1, next: next ? next * -1 : next };
+
+    case '÷':
+    case '×':
+    case '-':
+    case '+':
+      if (!total) {
+        return {};
+      }
+      return { operation: buttonName };
+
+    case '=':
+      if (total && next) {
+        total = operate(total, next, operation);
+        return { total, next: null, operation: buttonName };
+      } if (total) {
+        return { total, operation: buttonName };
+      }
+      return {};
 
     default:
-      cTotal = operate(cTotal, cNext, cOperation);
-      break;
+      if (total && buttonName === '%') {
+        return { total: operate(total, next, buttonName), operation: '=' };
+      }
+      if (buttonName === '%') {
+        return {};
+      }
+
+      if (!Number.isNaN(buttonName)) {
+        if (operation === '=') {
+          return { total: buttonName, operation: null };
+        }
+        if (operation) {
+          return { next: next ? next + buttonName : buttonName };
+        }
+        return { total: total ? total + buttonName : buttonName };
+      }
   }
-  return { cTotal, cNext, cOperation };
+  return {};
 };
 
 export default calculate;
