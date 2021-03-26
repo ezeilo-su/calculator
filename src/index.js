52e1aebb8b6a00e19b3e6b9fe303e412e9d06262
ezeilo-su <ezeilo.su@gmail.com>
2021-03-26 12:16:23 +0100

    Setup React Route

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './components/App';
+import Routes from './Routes';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <Routes />
+  </React.StrictMode>,
   document.getElementById('root'),
 );
